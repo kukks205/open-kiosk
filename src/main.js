d6aeb81f71eba89e07bd3f3cd3c59815b71af0e6
Suputtana <kukks205@gmail.com>
2023-03-28 11:18:36 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, './src/preload.js')
+    }
+  })
+
+  win.loadFile('./src/pages/index.html')
+  win.maximize()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
